ad134fb1ca8af932d508b3a138d6f5aeda7b15c4
Emily Aguirre <emgal.aguirre@gmail.com>
2020-10-27 13:30:55 -0500

    initial questions created

--- Develop/js/tracker.js
+++ Develop/js/tracker.js
@@ -14,7 +14,7 @@ function startApp() {
             {
                 type: "list",
                 message: " Welcome to the Employee Management System. How can I help you today?",
-                choices: ["Add emplyee, role, or department", "View employees, roles or departments", "Update employee roles", "End session"],
+                choices: ["Add emplyee, role, or department", "View employees, roles or departments", "End session"],
                 name: "initialChoice"
             }
         ]).then(answer => {
@@ -22,11 +22,49 @@ function startApp() {
                 addOption();
             } else if (answer.initialChoice === "View emplyee, role, or department") {
                 viewOption();
-            } else if (answer.initialChoice === "Update employee roles") {
-                updateEmployee();
             } else if (answer.initialChoice === "End session") {
                 console.log("Session complete. Thanks for using the Emplyeement System!")
                 connection.end();
             };
         });
+};
+
+function addOption() {
+    inquierer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to add a new employee, role or department?",
+                choices: ["New Employee", "New Role", "New Department"],
+                name: "addedOption"
+            }
+        ]).then(answer => {
+            if (answer.addedOption === "New Employee") {
+                newEmployee();
+            } else if (answer.initialChoice === "New Role") {
+                newRole();
+            } else if (answer.initialChoice === "New Department") {
+                newDepartment();
+            }
+        })
+};
+
+function viewOption() {
+    inquierer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to view employee, role or depepartment directory?",
+                choices: ["Employee", "Role", "Department"],
+                name: "viewChoice"
+            }
+        ]).then(answer => {
+            if (answer.viewChoice === "Employee") {
+                viewEmployee();
+            } else if (answer.viewChoice === "Role") {
+                viewRole();
+            } else if (answer.viewChoice === "Department") {
+                viewDepartment();
+            }
+        })
 };
